76e51b346d7e739f0a89f2566bd61e1c5bfc850b
vishnudhruv <vishnu.oneteamsolutions@gmail.com>
2024-08-05 15:29:23 +0530

    second final

---
+++ listeven.py
@@ -0,0 +1,11 @@
+a=[]
+n= int(input("Enter the number of elements : "))
+print("Enter the elements : ")
+for i in range(0,n):
+	k=int(input())
+	a.append(k)
+for i in range(0,n):
+	if a[i]%2==0:
+		print(a[i])
+
+
